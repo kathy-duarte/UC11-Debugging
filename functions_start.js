77d67b18c01be5b7524b8a6d01e8e56c1854afbc
Keilani K Burak <kb17180@srcs.loc>
2017-02-08 09:25:45 -0800

    Finished User Story 4 and 5

--- functions_start.js
+++ functions_start.js
@@ -60,8 +60,8 @@ function multTable(row, col) {
  * @returns {number}
  */
 function factorial(n) {
-    var result = 0;
-    for (var i = n; i > 1; i--) {
+    var result = 1;
+    for (var i = n; i > 0; i--) {
         result = result * i;
     }
     return result;
@@ -75,7 +75,7 @@ function factorial(n) {
  */
 function combinations(n, k) {
     var c;
-    c = factorial(n) / factorial(k) * (factorial(n - k));
+    c = factorial(n) / (factorial(k) * (factorial(n - k)));
     return c;
 }
 
